8b0a1c2b9862016369433eabef288923982e4bb4
hoangdl <hoangledinh65@gmail.com>
2022-07-26 15:37:41 +0700

    update

--- security/kms.tf
+++ security/kms.tf
@@ -7,6 +7,11 @@ resource "aws_kms_key" "a" {
   }
 }
 
+resource "aws_kms_alias" "a" {
+  name          = "alias/my-key-alias"
+  target_key_id = aws_kms_key.a.key_id
+}
+
 output "key-id" {
   value = aws_kms_key.a.id
 }
